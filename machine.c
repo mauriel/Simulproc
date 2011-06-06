585df76cf2f18ad2e6f61451d0a917f82d3b7e58
Hanno Bever <bever@v307.(none)>
2011-06-06 15:51:21 +0200

    print cpu

--- machine.c
+++ machine.c
@@ -13,3 +13,35 @@ void load_program(Machine *pmach,
 	pmach->_dataend = dataend;
 }
 
+
+/**
+ * Fonction d'affichage des registres
+ * */
+void print_cpu(Machine *pmach);
+{
+   int i;
+
+   printf("PC : 0x%hu\n",pmach->_pc);
+
+   printf("CC : ")
+   switch(pmach->_cc){
+	case CC_U :
+		putchar('U'); break;
+	case CC_Z :
+		putchar('Z'); break;
+	case CC_P :
+		putchar('P'); break;
+	case CC_N :
+		putchar('N'); break;
+
+   }
+  putchar('\n');
+  
+	
+  for(i=0;i<=13;i++){
+      printf("R%d : 0x%x %d\n",i,pmach->_registers[i],pmach->_registers[i]);
+    }
+   printf("SP : %x %d\n",pmach->_sp, pmach->_sp);
+}
+
+
