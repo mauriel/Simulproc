da2bca3a518036f43dbc94faa64e29dba4025f66
Hanno Bever <bever@v307.(none)>
2011-06-06 17:41:59 +0200

    all print

--- machine.c
+++ machine.c
@@ -1,4 +1,6 @@
 #include "machine.h"
+#include "exec.h"
+#include "debug.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
@@ -11,6 +13,8 @@ void load_program(Machine *pmach,
 	pmach->_data = datas;
 	pmach->_datasize = datasize;
 	pmach->_dataend = dataend;
+	// mettre PC a 0 ou pas ?
+	//pmach->_pc = 0;  
 }
 
 /**
@@ -19,9 +23,8 @@ void load_program(Machine *pmach,
 void print_cpu(Machine *pmach);
 {
    int i;
-
+   printf("CPU:\n\n");
    printf("PC : 0x%hu\n",pmach->_pc);
-
    printf("CC : ")
    switch(pmach->_cc){
 	case CC_U :
@@ -32,15 +35,13 @@ void print_cpu(Machine *pmach);
 		putchar('P'); break;
 	case CC_N :
 		putchar('N'); break;
-
    }
   putchar('\n');
-  
-	
+
   for(i=0;i<=13;i++){
       printf("R%d : 0x%x %d\n",i,pmach->_registers[i],pmach->_registers[i]);
     }
-   printf("SP : %x %d\n",pmach->_sp, pmach->_sp);
+   printf("SP : 0x%x %d\n",pmach->_sp, pmach->_sp);
 }
 
 void read_program(Machine *mach, const char *programfile)
@@ -50,22 +51,48 @@ void read_program(Machine *mach, const char *programfile)
 
 void dump_memory(Machine *pmach)
 {
-  
+  int i;
+  for(i =0; i < pmach->_textsize; i++)
+    {
+      printf("%x\n", pmach->_text[i]->_raw);
+    }
+  for(i =0; i < pmach->_datasize; i++)
+    {
+      printf("%x\n", pmach->_data[i]);
+    }
+  // TODO mettre les memes données dans le fichier demandé
 }
 
 
-void print_program(Machine *pmach)
-{
-  
+void print_program(Machine *pmach){
+  int i;
+  printf("Instructions:\n");
+  for(i =0; i < pmach->_textsize; i++)
+    {
+      printf("0x%x : 0x%x   ", i, pmach->_text[i]->instr_absolute->_address);
+      print_instruction(pmach->_text[i],pmach->_text[i]->instr_absolute->_address);
+      printf("\n");
+    }
 }
 
 
 void print_data(Machine *pmach)
 {
-  
+  int i;
+  printf("Data:\n");
+  for(i =0; i < pmach->_datasize; i++)
+    {
+      printf("0x%x : 0x%x %d \n", i, pmach->_data[i], pmach->_data[i]);
+    }
 }
 
 void simul(Machine *pmach, bool debug)
 {
-  
+  bool debugActive = debug;
+  while (pmach->_pc < pmach->_textsize){
+	if (debugActive)
+		debugActive = debug_ask(Machine *pmach);
+	decode_execute(pmach, pmach->_text[pmach->_pc]);
+	pmach->_pc++;	
+  }
 }
