0a4be884157d0fa44618a614522cbd3b073ed9b8
clem <wirth@polytech.unice.fr>
2011-06-07 10:18:03 +0200

    Instruction termine

--- exec.c
+++ exec.c
@@ -1,5 +1,13 @@
 #include "exec.h"
 
 bool decode_execute(Machine *pmach, Instruction instr) {
+	
+	
+	
 	return false;
+}
+
+void trace(const char *msg, Machine *pmach, Instruction instr, unsigned addr) {
+	printf("%s: 0x%04x: ",msg,addr);
+	print_instruction(instr);
 }
