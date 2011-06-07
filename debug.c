7dba44c1ac24f8d50a9f772dee81b3a58a170eaf
Hanno Bever <bever@v307.(none)>
2011-06-07 15:13:29 +0200

    debug Commentaires

--- debug.c
+++ debug.c
@@ -1,8 +1,25 @@
+/*!
+ * \file debug.c
+ * \brief Fonctions de mise au point interactive.
+ */
+
+
 #include "debug.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 
+//! Dialogue de mise au point interactive pour l'instruction courante.
+/*!
+ * Cette fonction gère le dialogue pour l'option \c -d (debug). Dans ce mode,
+ * elle est invoquée après l'exécution de chaque instruction.  Elle affiche le
+ * menu de mise au point et on exécute le choix de l'utilisateur. Si cette
+ * fonction retourne faux, on abandonne le mode de mise au point interactive
+ * pour les instructions suivantes et jusqu'à la fin du programme.
+ * 
+ * \param mach la machine/programme en cours de simulation
+ * \return vrai si l'on doit continuer en mode debug, faux sinon
+ */
 bool debug_ask(Machine *pmach){
 	char c = 'a';
 	char d;
